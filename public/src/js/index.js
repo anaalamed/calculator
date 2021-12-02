ba32f367958c4c175d96f66fa5af83d85d08788c
Ana Levit <anaalamed@gmail.com>
2021-12-02 16:16:14 +0200

    +/- btn

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -8,27 +8,38 @@ const equalBtn = document.querySelector("#equal");
 
 let a = null;
 let b = null;
+let sign = '+';
 let operator;
-
 let reset;
 
 // click on numbers butons 
 const onClickNum = (e) => {
     console.log("click on numbers butons ");
-    // reset values after A and creating B
     console.log(reset)
+    console.log("sign:", sign)
+
+    // reset values after A and creating B
     if (a !== null && reset === true || values.innerText == 0) {
-        console.log('ipus');
-        values.innerText = null;
-        reset = false;
+    // if (a !== null && reset === true ) {
+        // if(values.innerText != -0 ) {
+            console.log('ipus');
+            values.innerText = null;
+            reset = false;
+        // } 
     }
 
-    
     if (values.innerText.length <9) {
         console.log('boom');
-        console.log(values.innerText)
         values.innerText += e.target.innerText;
+        console.log(values.innerText);
     }
+
+    if (sign === '-') {
+        // values.innerText = "-" + e.target.innerText;
+        values.innerText = "-" + values.innerText;
+        sign = "+";
+    }
+    // values.innerText = new Intl.NumberFormat().format((values.innerText))
 }
 
 for (let btn of numBtns ) {
@@ -38,13 +49,13 @@ for (let btn of numBtns ) {
 // click on operator - a, op
 for (let op of operators ) {
     op.addEventListener("click", (e) => {
-        console.log("operator");
         a = values.innerText;
         operator = e.target.innerText;
         
         console.log('a', a);
-        console.log(operator);
+        console.log("operator", operator);
         reset = true;
+        sign = "+";
         // values.innerText = null;
     });
 }
@@ -62,14 +73,23 @@ equalBtn.addEventListener("click", () => {
             case "+": res = Number(a) + Number(b); break;
             case "-": res = Number(a) - Number(b); break;
             case "x": res = Number(a) * Number(b); break;
-            case "÷": res = Number(a) / Number(b); break;
+            // case "÷": res = (Number(a) / Number(b)); break;
+            case "÷": res = (Number(a) / Number(b)).toString().substring(0,9); break;
         }
-        console.log(res)
-        console.log(typeof(res))
+        
+        
+        if(res.toString().length > 9 ) {
+            res = new Intl.NumberFormat('en-US', { notation: "scientific" }).format(res);
+        }
 
-    
+        console.log(typeof(res))
+        // let resFormatted = new Intl.NumberFormat().format(res);
+        // console.log(typeof(resFormatted))
+        // values.innerText = resFormatted;
         values.innerText = res;
         a = res;
+        b = null;
+        sign = "+";
     }
 })
 
@@ -80,11 +100,42 @@ equalBtn.addEventListener("click", () => {
 
 // AC button
 const clearBtn = document.querySelector("#clear");
-
 clearBtn.addEventListener("click", () => {
     values.innerText = 0;
     a = null;
     b = null;
+    sign = '+';
+})
+
+// +/- button
+const signBtn = document.querySelector("#changeSign");
+signBtn.addEventListener("click", () => {
+    console.log("sign:", sign)
+
+
+    if (values.innerText.toString().includes("-")) {
+        sign = "-";
+    }
+
+    if (sign === "+") {
+        sign = "-"
+        values.innerText = "-"+(values.innerText);
+    }
+    else if (sign === '-') {
+        sign = '+';
+        values.innerText = (values.innerText).substring(1);
+    } 
+
+    // if creating b
+    if (a !== null && reset === true ) {
+        // console.log('sign');
+        values.innerText = "-0";
+        sign = "-";
+        reset = false;
+    }
+    console.log("sign:", sign)
+
+    reset = false;
 })
 
 
